ac6a1ad288a83c2dbb81ca5d4d741742a6c33cb1
Menjith P <70083973+menjithp@users.noreply.github.com>
2023-08-15 16:58:36 +0530

    Create slice.js

---
+++ src/store/slice.js
@@ -0,0 +1,61 @@
+import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
+import { MOVIE_API,MOVIE_API_KEY } from '../properties'
+
+const initialState = {
+  filtered_upcoming_movies:null,
+  error:"",
+  all_upcoming_movies:null,
+  page:"list_page",
+  isLoading:true
+}
+export const fetchContent = createAsyncThunk(
+    'content/fetchContent',
+     async() => {
+        const options = {
+            method: 'GET',
+            headers: {
+            accept: 'application/json',
+            Authorization: MOVIE_API_KEY
+            }
+        };
+        let data=await fetch(MOVIE_API,options)
+        data= await data.json()
+        return data
+        
+    }
+  )
+
+export const slice = createSlice({
+  name: 'upcoming_movie_list_reducer',
+  initialState,
+  reducers: {
+    update_filtered_upcoming_movies: (state,action) => {
+        console.log("in reducer",action)
+      state.filtered_upcoming_movies = action.payload
+    },
+    set_all_upcoming_movies: (state,action) => {
+        state.all_upcoming_movies = action.payload
+        state.filtered_upcoming_movies = action.payload
+      },
+      set_error_state:(state,action)=>{
+          state.error=action.payload
+      }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchContent.pending, (state) => {
+        state.isLoading = true
+    })
+    builder.addCase(fetchContent.fulfilled, (state, action) => {
+        state.all_upcoming_movies = action.payload
+        state.filtered_upcoming_movies = action.payload
+        state.isLoading = false
+    })
+    builder.addCase(fetchContent.rejected, (state, action) => {
+      state.error = action.error.message
+      state.isLoading = false
+    })
+  },
+})
+
+export const {update_filtered_upcoming_movies,set_all_upcoming_movies,set_error_state} = slice.actions
+export default slice.reducer
