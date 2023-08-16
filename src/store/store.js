c352103d20c350f0e536b02076a247a14f2b4842
Menjith P <70083973+menjithp@users.noreply.github.com>
2023-08-16 10:07:07 +0530

    Delete src directory

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import upcoming_movie_list_reducer_slice from './slice'
-
-export const store = configureStore({
-  reducer: {
-    upcoming_movie_list_reducer:upcoming_movie_list_reducer_slice
-  },
-})
