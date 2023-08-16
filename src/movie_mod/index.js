aede1d5af127f0560f46a3b1465f8e961ee646dc
Menjith P <70083973+menjithp@users.noreply.github.com>
2023-08-16 12:53:06 +0530

    Update index.js

--- src/movie_mod/index.js
+++ src/movie_mod/index.js
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect,useRef } from "react";
+import { useEffect } from "react";
 import MovieList from "./movie_comp/movie_list";
+import { MoreMenu } from "./icons";
 //import properties
 import { fetchContent } from "../store/slice";
 import Loader from "./Components/list_loader";
@@ -19,6 +20,13 @@ export default () => {
       {movies?.results?.length ? <MovieList movie_list={movies} />:(!state.isLoading && !state.error && <p className="blue-text">No Movies To Display </p>)}
       {state.isLoading && <Loader />}
       {state.error && <p className="red-text">state.error</p>}
+      {state.page_no <= state.total_pages  && (<div className="w-100">
+          <button data-tooltip-id="my-tooltip" data-tooltip-content="Load more data" className="fetch-more" onClick={() => dispatch(fetchContent(state.page_no + 1))}>
+           <MoreMenu />
+          </button>
+          </div>
+        )}
+     
     </>
   );
 };
